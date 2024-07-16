6d1004daaf5d6f68018bff6bb7c39041d4dff024
lrnzdmd <170991996+lrnzdmd@users.noreply.github.com>
2024-07-16 03:56:08 +0200

    added a function to return a different players mark for player 1 or 2

--- tictactoe.js
+++ tictactoe.js
@@ -6,11 +6,27 @@ function Gameboard() {
 
     this.printBoard = () => console.log(board);
 
-    this.addMark = function(mark, column, row) {
-        board[column][row] = mark;
+    this.addMark = function(mark, row, column) {
+        board[row][column] = mark;
     } 
+
+    this.resetBoard = function() {
+        board.forEach(row => row.fill(" "));
+    }
+}
+
+// Player object constructor
+
+function Player(name, playerNumber) {
+    this.name = name;
+    this.getMark = function () {
+        if (playerNumber === 1) { 
+            return "✖"; } 
+            else { 
+                return "〇"; }
+    }
 }
 
 const board = new Gameboard();
-board.addMark("X", 2,2)
+
 board.printBoard();
